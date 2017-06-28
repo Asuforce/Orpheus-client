e380fa95ffbc96ed4ba66af79287b1d770a6ecf2
asuforce <asuforcegt@hotmail.co.jp>
2017-06-28 15:07:00 +0900

    create root page

---
+++ components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="footer">
+    <small>
+      This is NUXT with Rails API application.
+      by <a href="https://github.com/Asuforce/">@Asuforce</a>
+    </small>
+  </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="container">
+      <nav class="navbar navbar-fixed-top navbar-inverse">
+        <nuxt-link to="/" id="logo">Orpheus</nuxt-link>
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <li><nuxt-link to="/">Home</nuxt-link></li>
+            <li><nuxt-link to="/help">Help</nuxt-link></li>
+            <li>login function</li>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
