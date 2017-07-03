39d16b214c64c0a43afacec71c945d4c14f1b380
asuforce <asuforcegt@hotmail.co.jp>
2017-07-03 12:39:18 +0900

    create user list page

---
+++ components/user/Field.vue
@@ -0,0 +1,15 @@
+<template lang='pug'>
+div
+  .card-content
+    .media
+      .media-left
+        figure.image.is-50x50
+          slot(name='image')
+      .media-content
+        slot(name='name')
+        slot(name='id')
+
+//- - if current_user.admin? && !current_user?(user)
+//-   |  |
+//-   = link_to "delete", user, method: :delete, data: { confirm: "You sure?" }
+</template>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -7,15 +7,23 @@
         //- <%= will_paginate %>
 
         ul
-          li(v-for="user in users") {{ user.name }}
+          li(v-for="user in users")
+            user-field
+              img(slot='image', v-bind:src="user.img", v-bind:alt="user.name")
+              p.title.is-4(slot='name') {{ user.name }}
+              p.subtitle.is-6(slot='id') @{{ user.name }}
 
         //- <%= will_paginate %>
 </template>
 
 <script>
 import axios from 'axios'
+import UserField from '~components/user/Field.vue'
 
 export default {
+  components: {
+    UserField
+  },
   asyncData () {
     return axios.get('http://localhost:3001/api/users').then(res => res.data)
   },
